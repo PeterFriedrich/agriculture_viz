79e63e8acf8a36a7cb8b4fd67cb2bd1dba16117d
Peter <peter.friedrich.one@gmail.com>
2023-11-09 21:32:18 -0700

    added slider demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,42 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+from datetime import time, datetime
+
+# slider demo, headers and subheaders.
+st.header('st.slider')
+
+# example 1
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+# example 2, range slider just add more
+st.subheader('Range slider')
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# example 3, sometimes putting value is easier
+st.subheader('Range time slider')
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+# example 4
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
+
 # new button script, can you split
 # up the streamlit script?
 st.header('st.button')
