6cdbd2e72cfe972bf5970d4f9dfc0c459897b7d2
Peter <peter.friedrich.one@gmail.com>
2023-11-11 16:46:50 -0700

    adding weird profiling

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,16 @@ import altair as alt
 
 from datetime import time, datetime
 
+import ydata_profiling
+from streamlit_pandas_profiling import st_profile_report
+
+# Streamlit Components
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+pr = df.profile_report()
+st_profile_report(pr)
+
 # st.checkbox app. Puts up checkboxes
 st.header('st.checkbox')
 st.write('What would you like to order?')
