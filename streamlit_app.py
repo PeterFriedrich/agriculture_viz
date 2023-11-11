9d3284fc9cdeb9b3c9d3b904ee1a850c8b5b820c
Peter <peter.friedrich.one@gmail.com>
2023-11-11 11:43:32 -0700

    added checkboxes

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,22 @@ import altair as alt
 
 from datetime import time, datetime
 
+# st.checkbox app. Puts up checkboxes
+st.header('st.checkbox')
+st.write('What would you like to order?')
 
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+    st.write("Great! Here's some more :icecream")
+
+if coffee:
+    st.write("Okay, here's some coffee :coffee")
+
+if cola:
+    st.write("Here you go :pop")
 
 # st.multiselect widget select?
 st.header('st.multiselect')
