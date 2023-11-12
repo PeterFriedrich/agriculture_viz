cde2a8df0fd01dc6a16b9f425bda3a5c86579f38
Peter <peter.friedrich.one@gmail.com>
2023-11-11 18:00:48 -0700

    theming entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,24 @@ import altair as alt
 
 from datetime import time, datetime
 
+# streamlit themes??
+st.title('Customizing the theme of streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFF"
+        font="monospace"
+        """)
+
+# fancy sidebar slider
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write("Selected number from slider widget is:", number)
+
 # streamlit latex
 st.header('st.latex')
 
